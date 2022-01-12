edf5ef489fb01358ce6215609c49a4da7a3b5787
Anton Rybakov <anryoshi@gmail.com>
2022-01-12 06:47:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# right_lick dojo
---
+++ example_project/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string input;
+    std::cin >> input;
+
+    int a = 4;
+    
+    std::cout << input;
+    return 0;
+}
